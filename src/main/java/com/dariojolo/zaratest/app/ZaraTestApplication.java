eab2d00d36a5fe2f832fad3dcc24c200678aeb34
Dario Jolodovsky <dariojolo@gmail.com>
2021-07-28 15:27:50 -0300

    Project start

---
+++ src/main/java/com/dariojolo/zaratest/app/ZaraTestApplication.java
@@ -0,0 +1,13 @@
+package com.dariojolo.zaratest.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaraTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZaraTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dariojolo/zaratest/app/ZaraTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dariojolo.zaratest.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaraTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

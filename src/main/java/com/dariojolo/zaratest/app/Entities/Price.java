d7bb3b0a685e791af9314f6bca4a895bcb96fd71
Dario Jolodovsky <dariojolo@gmail.com>
2021-07-31 21:26:34 -0300

    add builder to price entity

--- src/main/java/com/dariojolo/zaratest/app/Entities/Price.java
+++ src/main/java/com/dariojolo/zaratest/app/Entities/Price.java
@@ -1,12 +1,18 @@
 package com.dariojolo.zaratest.app.Entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "prices")
 public class Price{
 
